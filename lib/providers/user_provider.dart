90ac7a4a17dde710341e565c0d58077fd537b172
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-02 00:40:10 +0530

    UserProvider + UserRepository config

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,3 +1,26 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:chatapp_amplify/repositories/user_repository.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 
-class UserProvider with ChangeNotifier {}
+class UserProvider with ChangeNotifier {
+
+  final _userRepository = UserRepository();
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  void _setIsLoading(bool value){
+    _isLoading = value;
+    notifyListeners();
+  }
+
+  //response to usermail
+  Future <Either<String, SignUpResult>> signUp(
+    String username, email, password) async{
+      _setIsLoading(true);
+      final response = await _userRepository.signUp(username, email, password);
+      _setIsLoading(false);
+      return response;
+  }  
+}
+
--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,1 +1,27 @@
-class UserRepository {}
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:dartz/dartz.dart';
+
+class UserRepository {
+
+  //aws amplify signup setup
+  Future <Either<String, SignUpResult>> signUp(
+    String username, email, password) async{
+
+    // email for user login  
+    final Map<AuthUserAttributeKey, String> userAttributes = {
+      AuthUserAttributeKey.email: email
+    };
+
+    try {
+      final result = await Amplify.Auth.signUp(
+        username: username, 
+        password: password,
+        options: SignUpOptions(userAttributes: userAttributes)
+        );
+        return right(result);
+    } on  AuthException catch (e) {
+      return left(e.message);
+    }
+  }
+}
