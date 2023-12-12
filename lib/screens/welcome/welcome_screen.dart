9007a93d251828888bd4b47c661f5a0f6267e54c
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-12 21:06:54 +0530

    WelcomeScreen

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,6 +1,9 @@
 import 'package:chatapp_amplify/constants.dart';
+import 'package:chatapp_amplify/providers/user_provider.dart';
 import 'package:chatapp_amplify/screens/auth/signin_or_signup_screen.dart';
+import 'package:chatapp_amplify/screens/messages/message_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -35,42 +38,74 @@ class WelcomeScreen extends StatelessWidget {
               ),
             ),
             const Spacer(flex: 3),
-            FittedBox(
-              child: TextButton(
-                  onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SigninOrSignupScreen(),
-                        ),
-                      ),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Skip",
-                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .bodyLarge!
-                                  .color!
-                                  .withOpacity(0.8),
+            FutureBuilder(
+                future: context.read<UserProvider>().checkedLogedInUser(),
+                builder: (context, snapshot) {
+                  Future.delayed(Duration(seconds: 1), () {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.data != null) {
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MessagesScreen(),
                             ),
-                      ),
-                      const SizedBox(width: defaultPadding / 4),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 16,
-                        color: Theme.of(context)
-                            .textTheme
-                            .bodyLarge!
-                            .color!
-                            .withOpacity(0.8),
-                      )
-                    ],
-                  )),
-            )
+                            (route) => false,
+                          );
+                        }
+                        );
+                      }
+                    }
+                  });
+                   return SkipButton();
+                }),
+       
           ],
         ),
       ),
+    );
+  }
+}
+
+class SkipButton extends StatelessWidget {
+  const SkipButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      child: TextButton(
+          onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SigninOrSignupScreen(),
+                ),
+              ),
+          child: Row(
+            children: [
+              Text(
+                "Skip",
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context)
+                          .textTheme
+                          .bodyLarge!
+                          .color!
+                          .withOpacity(0.8),
+                    ),
+              ),
+              const SizedBox(width: defaultPadding / 4),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 16,
+                color: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .color!
+                    .withOpacity(0.8),
+              )
+            ],
+          )),
     );
   }
 }
