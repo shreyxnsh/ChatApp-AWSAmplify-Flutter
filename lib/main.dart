6902316d182d9769f109004ce4f69b4d0398d20e
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-13 22:51:42 +0530

    MaterialApp => GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:chatapp_amplify/providers/user_provider.dart';
 import 'package:chatapp_amplify/screens/welcome/welcome_screen.dart';
 import 'package:chatapp_amplify/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
@@ -43,7 +44,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'The Flutter Way',
       debugShowCheckedModeBanner: false,
       theme: lightThemeData(context),
