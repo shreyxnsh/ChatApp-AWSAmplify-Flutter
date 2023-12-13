65b3fddfab8d149e83bb10b11c4dd674ca678867
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-13 22:51:23 +0530

    Signout implemented

--- lib/screens/messages/message_screen.dart
+++ lib/screens/messages/message_screen.dart
@@ -1,5 +1,11 @@
 import 'package:chatapp_amplify/constants.dart';
+import 'package:chatapp_amplify/providers/user_provider.dart';
+import 'package:chatapp_amplify/screens/auth/sign_in_screen.dart';
+import 'package:chatapp_amplify/shared/extension.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
 
 import '../../models/ChatMessage.dart';
 import 'components/chat_input_field.dart';
@@ -16,7 +22,11 @@ class MessagesScreen extends StatelessWidget {
         title: const Text("Flutter Dev Chat"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () async {
+              final response = await context.read<UserProvider>().signOut();
+              response.fold((error) => context.showError(error),
+                  (result) => Get.to(()=> const SignInScreen()) );
+            },
             icon: const Icon(Icons.logout_outlined),
           ),
         ],
