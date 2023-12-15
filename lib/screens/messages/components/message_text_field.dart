ab98099aee6b49e3ca49dbd6702d01ce03f9f4ca
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-16 00:04:29 +0530

    message repository

--- lib/screens/messages/components/message_text_field.dart
+++ lib/screens/messages/components/message_text_field.dart
@@ -1,5 +1,11 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:chatapp_amplify/models/Message.dart';
+import 'package:chatapp_amplify/providers/message_provider.dart';
+import 'package:chatapp_amplify/providers/user_provider.dart';
+import 'package:chatapp_amplify/shared/extension.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
 class MessageTextField extends StatefulWidget {
   const MessageTextField({
@@ -11,13 +17,14 @@ class MessageTextField extends StatefulWidget {
 }
 
 class _MessageTextFieldState extends State<MessageTextField> {
-
   late final TextEditingController _messageController;
+  late AuthUser _currentUser;
 
   @override
   void initState() {
     // TODO: implement initState
     _messageController = TextEditingController();
+    _currentUser = context.read<UserProvider>().currentUser!;
     super.initState();
   }
 
@@ -27,13 +34,30 @@ class _MessageTextFieldState extends State<MessageTextField> {
     _messageController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: _messageController,
       textInputAction: TextInputAction.send,
-      onFieldSubmitted: (value) {
-        
+      
+      onFieldSubmitted: (value) async {
+        if (value.isNotEmpty) {
+          final message = Message(
+              userId: _currentUser.userId,
+              username: _currentUser.username,
+              message: _messageController.text,
+              type: "Message",
+              createdAt: DateTime.now().toString());
+              print(message);
+          final response =
+              await context.read<MessageProvider>().sendMessage(message);
+          response.fold((error) => context.showError(error), (resMessage) {
+            if (resMessage != null ) {
+              _messageController.clear();
+            }
+          });
+        }
       },
       decoration: InputDecoration(
         hintText: "Type message",
