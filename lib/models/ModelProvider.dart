484d0da5b3679c17c2c5f8734ba1110785ee1604
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-16 00:04:04 +0530

    created message providers

--- lib/models/ModelProvider.dart
+++ lib/models/ModelProvider.dart
@@ -19,25 +19,25 @@
 
 // ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
 
-import 'package:amplify_core/amplify_core.dart';
+import 'package:amplify_core/amplify_core.dart' as amplify_core;
 import 'Message.dart';
 import 'Todo.dart';
 
 export 'Message.dart';
 export 'Todo.dart';
 
-class ModelProvider implements ModelProviderInterface {
+class ModelProvider implements amplify_core.ModelProviderInterface {
   @override
   String version = "f2d1416276d540b5ec985a6bbd2dfef8";
   @override
-  List<ModelSchema> modelSchemas = [Message.schema, Todo.schema];
-  static final ModelProvider _instance = ModelProvider();
+  List<amplify_core.ModelSchema> modelSchemas = [Message.schema, Todo.schema];
   @override
-  List<ModelSchema> customTypeSchemas = [];
+  List<amplify_core.ModelSchema> customTypeSchemas = [];
+  static final ModelProvider _instance = ModelProvider();
 
   static ModelProvider get instance => _instance;
   
-  ModelType getModelTypeByModelName(String modelName) {
+  amplify_core.ModelType getModelTypeByModelName(String modelName) {
     switch(modelName) {
       case "Message":
         return Message.classType;
@@ -47,4 +47,11 @@ class ModelProvider implements ModelProviderInterface {
         throw Exception("Failed to find model in model provider for model name: " + modelName);
     }
   }
+}
+
+
+class ModelFieldValue<T> {
+  const ModelFieldValue.value(this.value);
+
+  final T value;
 }
